bae3c5777be2f7afa4f9bf5741c0283f4d34c87f
Lawrence Sithole <lawrence.sithole1@outlook.com>
2023-07-20 22:20:54 +0100

    get day and month

---
+++ script.js
@@ -0,0 +1,13 @@
+function runChatBot() {
+    const response = document.getElementById("response");
+    const answer = document.getElementById("answer").value;
+    response.innerText = "Your name is " + answer;
+    const question = document.getElementById("question");
+    question.innerText = "When is your birthday?";
+  }
+  const answerForm = document.getElementById("answerForm");
+
+  answerForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    runChatBot();
+  });
